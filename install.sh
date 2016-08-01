afe508b30e160ea7f56db147f9d8d95e92bbcbe4
patrik <pat.danielsson@gmail.com>
2016-08-01 19:42:50 +0200

    Added i3, i3blocks, i3lock and termite config

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s /home/$USER/.dotfiles/xlock /home/$USER/.xlock
+ln -s /home/$USER/.dotfiles/i3 /home/$USER/.i3
+ln -s /home/$USER/.dotfiles/i3blocks.conf /home/$USER/.i3blocks.conf
+ln -s /home/$USER/.dotfiles/termite /home/$USER/.config/termite
