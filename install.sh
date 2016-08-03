2f053d8d36a078289a3cb21cd7c91fdd6cd8e423
Patrik Danielsson <pat.danielsson@gmail.com>
2016-08-03 10:47:02 +0200

    Added tmux

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s /home/$USER/.dotfiles/xlock /home/$USER/.xlock
 ln -s /home/$USER/.dotfiles/i3 /home/$USER/.i3
 ln -s /home/$USER/.dotfiles/i3blocks.conf /home/$USER/.i3blocks.conf
 ln -s /home/$USER/.dotfiles/termite /home/$USER/.config/termite
+ln -s /home/$USER/.dotfiles/tmux.conf /home/$USER/.tmux.conf
