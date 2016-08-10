4a2450119c1f46827678fa673050fa2fa6602d61
patrik <pat.danielsson@gmail.com>
2016-08-10 17:17:01 +0200

    Updated i3 and i3blocks

---
+++ i3/custom_dmenu_run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "[" > /home/patrik/.cache/custom_dmenu_run
+IFS=':' read -ra ADDR <<< "$CUSTOMPATH"
+for i in "${ADDR[@]}"; do
+    echo $i >> /home/patrik/.cache/custom_dmenu_run
+done
+
+bins=$(find /home/patrik/Programs/* -maxdepth 0 -type f && find /home/patrik/Programs/* -maxdepth 0 -type l)
+for i in $bins; do
+	echo $(basename $i) >> /home/patrik/.cache/custom_dmenu_run
+done
+temp=$(sort /home/patrik/.cache/custom_dmenu_run)
+rm /home/patrik/.cache/custom_dmenu_run
+touch /home/patrik/.cache/custom_dmenu_run
+for i in $temp; do
+	echo $i >> /home/patrik/.cache/custom_dmenu_run
+done
